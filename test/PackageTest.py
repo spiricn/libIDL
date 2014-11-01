a329ed2f0410bc5935576954d9c72db35b187806
Nikola Spiric <a href=mailto:nikola.spiric.ns@gmail.comnikola.spiric.ns@gmail.com/a>
2014-11-01 11:17:41 +0100

    More package tests added

--- test/PackageTest.py
+++ test/PackageTest.py
@@ -9,9 +9,9 @@ from test.TestBase import TestBase
 
 
 class PackageTest(TestBase):
-    def test_basic(self):
+    def test_package_import(self):
         '''
-        Basic imports tests.
+        Basic package import test.
         '''
         
         
@@ -30,6 +30,84 @@ class PackageTest(TestBase):
         
         interface  Test{
             void test(com.test1.module1.Src1Type arg1);
+        };
+        
+        typedef Src2Type;
+        '''
+        
+        src3 = '''
+        package other.test;
+        
+        import com;
+        
+        struct Test{
+            com.test1.module1.Src1Type field1;
+            
+            com.test2.module2.Src2Type field2;
+        };
+        
+        '''
+        
+        env = Environment()
+        
+        env.compileSource(src1, 'module1')
+        
+        env.compileSource(src2, 'module2')
+        
+        env.compileSource(src3, 'module3')
+        
+    def test_module_import(self):
+        '''
+        Basic module import test.
+        '''
+        
+        
+        src1 = '''
+        package com.test1;
+
+        typedef Src1Type;
+        
+        '''
+        
+        
+        src2 = '''
+        package com.test2;
+        
+        import com.test1.module1;
+        
+        interface Test{
+            void test(com.test1.module1.Src1Type arg1);
+        };
+        '''
+        
+        
+        env = Environment()
+        
+        env.compileSource(src1, 'module1')
+        
+        env.compileSource(src2, 'module2')
+        
+    def test_type_import(self):
+        '''
+        Basic type import test.
+        '''
+        
+        
+        src1 = '''
+        package com.test1;
+
+        typedef Src1Type;
+        
+        '''
+        
+        
+        src2 = '''
+        package com.test2;
+        
+        from com.test1.module1 import Src1Type;
+        
+        interface  Test{
+            void test(Src1Type arg1);
         };
         '''
         
@@ -59,30 +137,6 @@ class PackageTest(TestBase):
         except IDLError:
             pass
         
-        # Double import
-        src1 = '''\
-        package com.test;
-        
-        import com.unexisting;
-        '''
-        
-        src2 = '''
-        package com.test2;
-        
-        import com.test;
-        import com.test;
-        '''
-        
-        try:
-            env = Environment()
-            env.compileSource(src1, 'test1')
-            env.compileSource(src2, 'test2')
-            
-            self.fail()
-        except IDLError:
-            pass
-        
-        
         # Missing package declaration
         src = '''\
         
@@ -94,6 +148,30 @@ class PackageTest(TestBase):
             
             self.fail()
         except IDLError:
+            pass
+        
+        # Import type
+        # Missing package declaration
+        src1 = '''\
+        package com.test;
+        
+        typedef Src1Type;
+        '''
+        
+        src2 = '''\
+        package com.test;
+        
+        import com.test.module1.Src1Type;
+             
+        '''
+
+        try:        
+            env = Environment() 
+            env.compileSource(src1, 'module1')
+            env.compileSource(src2, 'module2')
+            
+            self.fail()
+        except:
             pass
     
 if __name__ == '__main__':
