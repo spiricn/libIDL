70a0224b4f71f70bba39f604e881b4c810ddb24b
Nikola Spiric <nikola.spiric@rt-rk.com>
2014-12-01 12:03:42 +0100

    ConfigTest.py module added & tests for inheritance & operator overloading configuration

---
+++ test/ConfigTest.py
@@ -0,0 +1,57 @@
+from idl.Environment import Environment
+import unittest
+
+from idl.IDLNotSupportedError import IDLNotSupportedError
+from idl.LangConfig import LangConfig
+from test.TestBase import TestBase
+
+
+class ConfigTest(TestBase):
+    def test_inheritance(self):
+        '''
+        Inheritance LangConfig test.
+        '''
+        
+        src = ''' \
+        package com.test;
+        
+        interface Base{
+        };
+        
+        interface Derived extends Base{
+        };
+        '''
+        
+        
+        try:
+            Environment(LangConfig(inheritance=False)).compileSource(src, 'module1')
+            self.fail()
+            
+        except IDLNotSupportedError:
+            pass
+        
+    def test_overload(self):
+        '''
+        Operator overloading LangConfig test.
+        '''
+        
+        src = ''' \
+        package com.test;
+        
+        interface Test{
+            void test(int32 arg1);
+            
+            void test();
+        };
+        '''
+        
+        
+        try:
+            Environment(LangConfig(operatorOverload=False)).compileSource(src, 'module1')
+            self.fail()
+            
+        except IDLNotSupportedError:
+            pass
+        
+if __name__ == '__main__':
+    unittest.main()
